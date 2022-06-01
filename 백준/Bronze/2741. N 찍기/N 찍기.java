90e06a30aad1329ad3f2fd9554920b2478f08d5a
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-06-01 23:44:02 +0900

    [Bronze III] Title: N 찍기, Time: 1152 ms, Memory: 33140 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i=1; i<=n; i++){
+            System.out.println(i);
+        }
+
+    }
+}
