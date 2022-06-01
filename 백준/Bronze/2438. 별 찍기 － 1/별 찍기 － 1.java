a7c76d487e5f483cc73b69efb064f5485c3b78f3
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-06-01 23:45:16 +0900

    [Bronze III] Title: 별 찍기 - 1, Time: 312 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int i,j;
+
+        for (i=1; i<=n; i++){
+            for (j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
