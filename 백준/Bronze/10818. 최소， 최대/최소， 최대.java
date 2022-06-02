422607206fcb21dd3f590fcec273e161885e4a99
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-06-02 23:15:32 +0900

    [Bronze III] Title: 최소, 최대, Time: 2552 ms, Memory: 322604 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int [] num = new int[n];
+        int max = num[0];
+        int min = num[0];
+        int i;
+
+        for (i=0; i<n; i++){
+            num[i] = sc.nextInt();
+        }
+        Arrays.sort(num);
+
+        System.out.println(num[0]+" "+num[num.length-1]);
+
+    }
+
+}
