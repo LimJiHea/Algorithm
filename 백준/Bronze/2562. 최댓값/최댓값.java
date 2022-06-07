c2f0968cc83c31520af51096a124fdedfbe052b2
LimJiHae <91642959+LimJiHea@users.noreply.github.com>
2022-06-07 23:00:59 +0900

    [Bronze II] Title: 최댓값, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int [] num = new int[9];
+
+        int max =num[0];
+        int idx =0;
+
+        for (int i=0; i<num.length; i++){
+            num[i] = sc.nextInt();
+            if (max < num[i]){
+                max = num[i];
+                idx = i+1;
+            }
+        }
+
+
+        System.out.println(max);
+        System.out.println(idx);
+
+    }
+
+}
